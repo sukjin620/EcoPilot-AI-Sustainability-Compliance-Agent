07b3050959b3e126b9065dcbc9535feeb718d100
Suk Jin <131194737+sukjin620@users.noreply.github.com>
2025-10-19 18:56:34 +0000

    EcoPilot initial set up

---
+++ amplify/backend/types/amplify-dependent-resources-ref.d.ts
@@ -0,0 +1,28 @@
+export type AmplifyDependentResourcesAttributes = {
+  "auth": {
+    "ecopilotfrontendff4615c6": {
+      "AppClientID": "string",
+      "AppClientIDWeb": "string",
+      "IdentityPoolId": "string",
+      "IdentityPoolName": "string",
+      "UserPoolArn": "string",
+      "UserPoolId": "string",
+      "UserPoolName": "string"
+    }
+  },
+  "function": {
+    "S3Trigger36830475": {
+      "Arn": "string",
+      "LambdaExecutionRole": "string",
+      "LambdaExecutionRoleArn": "string",
+      "Name": "string",
+      "Region": "string"
+    }
+  },
+  "storage": {
+    "RawData": {
+      "BucketName": "string",
+      "Region": "string"
+    }
+  }
+}
